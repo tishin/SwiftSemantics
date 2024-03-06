d61ecd416bfc070d6a62711826b10495685be6a0
Mikhail Tishin <michail.tishin@fayrix.com>
2024-03-06 23:18:56 +0300

    Bump SwiftSyntax version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftSyntax",
                  url: "https://github.com/apple/swift-syntax.git",
-                 from: "509.0.2"),
+                 from: "510.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
